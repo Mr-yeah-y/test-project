bb733a11902238ea929c4c092a66721d5def97ae
yeyonggang <yeyonggang@wepie.com>
2022-07-03 17:42:19 +0800

    第一次提交

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <lottory :items="items"></lottory>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, onBeforeMount, onMounted } from 'vue';
+import lottory from './components/lottory.vue'
+
+// ref reactive 都是将数据变成响应式数据
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    lottory
+  },
+  // setup 相当于 beforeCreate 和 created 在组件创建之前
+  setup() {
+    const items = [
+      {id:1, position: {}},
+      {id:2, position: {left:'60px',top:'0'}},
+      {id:3, position: {left:'120px', top:'0'}},
+      {id:4, position: {left:'120px',top:'60px'}},
+      {id:5, position: {left:'120px',top:'120px'}},
+      {id:6, position: {left:'60px',top:'120px'}},
+      {id:7, position: {left:'0',top:'120px'}},
+      {id:8, position: {left:'0',top:'60px'}},  
+    ]
+
+    return {items}
+  }
+});
+</script>
+
+<style>
+
+</style>
---
+++ src/components/lottory.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="hello">
+    <div class="lottory-wrap relative">
+      <div v-for="(item,index) in newItems" :key="index" class="lottory-item absolute" :style="item.position">{{item.id}}</div>
+      <div class="lottory-btn absolute" @click="onStartLottory(10)">抽奖</div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent} from 'vue';
+let $activeIndex = 0
+
+export default defineComponent({
+  name: 'lottory',
+  props: {
+    items: {
+      type: Array,
+      default: () => []
+    }
+  },
+  setup(props) {
+    const newItems = computed(() => {
+      function getNew<T> (item:T) {
+        return {...item, isActive:false}
+      }
+      return props.items.map(getNew)
+    })
+
+    const onStartLottory = (steps:number) => {
+      let maxTurns = 0
+    }
+
+    return {onStartLottory, newItems}
+  }
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.relative {
+  position: relative;
+}
+.absolute {
+  position: absolute;
+}
+.lottory-item {
+  width: 50px;
+  height: 50px;
+  background: red;
+  line-height: 50px;
+  text-align: center;
+  color: #fff;
+}
+.lottory-btn {
+  width: 50px;
+  height: 50px;
+  left: 60px;
+  top: 60px;
+  background: blue;
+  color: #fff;
+  font-weight: 600;
+  font-size: 20px;
+  text-align: center;
+  line-height: 50px;
+}
+
+</style>
